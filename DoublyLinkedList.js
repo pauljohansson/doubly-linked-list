46fbd811e678cc6c8679b2f067650281ce048300
pauljohansson <pralinen@gmail.com>
2022-03-28 15:32:27 +0200

    add classes

---
+++ DoublyLinkedList.js
@@ -0,0 +1,16 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.previous = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+    this.isCircular = false;
+  }
+}
