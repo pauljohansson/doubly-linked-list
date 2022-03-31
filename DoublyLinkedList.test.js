e7b5d101c143a9b249cf259f05697e154f2dae50
pauljohansson <pralinen@gmail.com>
2022-03-31 11:11:04 +0200

    first test for prependElement added

--- DoublyLinkedList.test.js
+++ DoublyLinkedList.test.js
@@ -1,1 +1,13 @@
 const DoublyLinkedList = require("./DoublyLinkedList");
+
+describe("#prependElement", () => {
+  describe("test 1: with an empty list", () => {
+    test("it adds the element to the beginning of the list", () => {
+      const dll = new DoublyLinkedList();
+      dll.prependElement(10);
+
+      expect(dll.head.value).toBe(10);
+      expect(dll.length).toBe(1);
+    });
+  });
+});
